d8924fa1dbd3f776eb1727f6743a672d19d4e05e
andresciceri <andresciceri@gmail.com>
2018-07-13 18:22:54 -0500

    feat: first commit & angular lesson 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavAppComponent } from './top-nav-app/top-nav-app.component';
+import { AsideLeftComponent } from './aside-left/aside-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavAppComponent,
+    AsideLeftComponent
   ],
   imports: [
     BrowserModule
-  ],
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/top-nav-app/top-nav-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-nav-app',
+  templateUrl: './top-nav-app.component.html',
+  styleUrls: ['./top-nav-app.component.styl']
+})
+export class TopNavAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
