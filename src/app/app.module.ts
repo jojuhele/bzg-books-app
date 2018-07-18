7c68d95087eecafc9d7d6d6c0e9c804cb3267bc6
andresciceri <andresciceri@gmail.com>
2018-07-18 10:03:16 -0500

    Class: 1-Components, Module & Inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopNavAppComponent } from './top-nav-app/top-nav-app.component';
 import { AsideLeftComponent } from './aside-left/aside-left.component';
+import { TopAsideLeftComponent } from './top-aside-left/top-aside-left.component';
+import { MenuAsideLeftComponent } from './menu-aside-left/menu-aside-left.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavAppComponent,
-    AsideLeftComponent
+    AsideLeftComponent,
+    TopAsideLeftComponent,
+    MenuAsideLeftComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
--- src/app/aside-left/aside-left.component.ts
+++ src/app/aside-left/aside-left.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "./user";
 
 @Component({
   selector: 'app-aside-left',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideLeftComponent implements OnInit {
 
+  user: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      name: "usuario",
+      lastname: "prueba",
+      username: "pruebaUser",
+      email: "usuario@prueba.com",
+      urlImage: "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+    }
   }
 
 }
